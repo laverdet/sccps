353bf510d444da5576c66810e434f2e17716bc18
Marcel Laverdet <marcel@laverdet.com>
2018-11-26 03:21:20 -0600

    Update terrain API, fix initialization bug

--- src/terrain.cc
+++ src/terrain.cc
@@ -7,17 +7,19 @@ terrain_t::terrain_t(room_location_t room) {
 	EM_ASM({
 		var roomName = Module.screeps.position.generateRoomName($0, $1);
 		var view = new Uint8Array(Module.buffer, $2, 625);
+		view.fill(0);
+		var terrain = Game.map.getRoomTerrain(roomName);
 		for (var xx = 0; xx < 50; ++xx) {
 			for (var yy = 0; yy < 50; ++yy) {
 				var val;
-				switch (Game.map.getTerrainAt(xx, yy, roomName)) {
-					case 'plain':
+				switch (terrain.get(xx, yy)) {
+					case 0:
 						val = 0;
 						break;
-					case 'wall':
+					case TERRAIN_MASK_WALL:
 						val = 1;
 						break;
-					case 'swamp':
+					case TERRAIN_MASK_SWAMP:
 						val = 2;
 						break;
 					default:
